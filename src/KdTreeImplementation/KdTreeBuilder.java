30718a576947592f90ab4dc5cee9fcb5c3aecc8e
Cate <caterina.munoz.v@gmail.com>
2014-05-29 00:41:33 -0400

    calculo de promedio entre coordenada maxima y minima

--- src/KdTreeImplementation/KdTreeBuilder.java
+++ src/KdTreeImplementation/KdTreeBuilder.java
@@ -18,21 +18,93 @@ public class KdTreeBuilder {
 		return null;
 		
 	}
-	public static int selectMeanX(ArrayList<Point2D> points){
+	/**selecciona el valor promedio entre la minima y la maxima coordenada x en el conjunto  
+	 * de puntos points
+	 * @param points
+	 * @return mean X
+	 */
+
+	public static float selectMeanX(ArrayList<Point2D> points){
+		int s=points.size();
+		//notar que si el tamaño es impar, se deja afuera al ultimo elemento
+		int halfSize=(int) Math.floor(s/2);
+		ArrayList<Double> possibleMaxs=new ArrayList<Double>();
+		ArrayList<Double> possibleMins=new ArrayList<Double>();
+		//compara pares de elementos consecutivos y los clasifica como
+		//posible maximo o posible minimo
+		for(int i=0;i<halfSize;i++){
+			if(points.get(2*i).getX()<=points.get(2*i+1).getX()){
+				possibleMaxs.add(points.get(2*i+1).getX());
+				possibleMins.add(points.get(2*i).getX());
+			}
+			else{
+				possibleMaxs.add(points.get(2*i).getX());
+				possibleMins.add(points.get(2*i+1).getX());
+			}	
+		}
+		//obtiene el maximo y el minimo de los potenciales conjuntos
+		float max=getMax(possibleMaxs);
+		float min=getMin(possibleMins);
+		//consideracion del ultimo elemento en caso de que el tamaño sea impar
+		if(s%2==1)
+			if(points.get(s-1).getX()<min)
+				min=(float) points.get(s-1).getX();
+			else if(points.get(s-1).getX()>max)
+				max=(float) points.get(s-1).getX();
+		return (max+min)/2;	
+	}
+
+	public static float selectMedianX(ArrayList<Point2D> points, int axis){
 		//TODO
 		return 0;
 	}
-	public static int selectMedianX(ArrayList<Point2D> points, int axis){
+	/**
+	 * todo analogo a selectMeanX
+	 * @param points
+	 * @return mean
+	 */
+	public static float selectMeanY(ArrayList<Point2D> points){
+		int s=points.size();
+		int halfSize=(int) Math.floor(s/2);
+		ArrayList<Double> possibleMaxs=new ArrayList<Double>();
+		ArrayList<Double> possibleMins=new ArrayList<Double>();
+		for(int i=0;i<halfSize;i++){
+			if(points.get(2*i).getY()<=points.get(2*i+1).getY()){
+				possibleMaxs.add(points.get(2*i+1).getY());
+				possibleMins.add(points.get(2*i).getY());
+			}
+			else{
+				possibleMaxs.add(points.get(2*i).getY());
+				possibleMins.add(points.get(2*i+1).getY());
+			}	
+		}
+		float max=getMax(possibleMaxs);
+		float min=getMin(possibleMins);
+		if(s%2==1)
+			if(points.get(s-1).getY()<min)
+				min=(float) points.get(s-1).getY();
+			else if(points.get(s-1).getY()>max)
+				max=(float) points.get(s-1).getY();
+		return (max+min)/2;	
+	}
+	public static float selectMedianY(ArrayList<Point2D> points, int axis){
 		//TODO
 		return 0;
 	}
-	public static int selectMeanY(ArrayList<Point2D> points){
-		//TODO
-		return 0;
+	
+	private static Float getMax(ArrayList<Double> possibleMaxs) {
+		Double candidate=possibleMaxs.get(0);
+		for(Double d: possibleMaxs)
+			if(d>candidate)
+				candidate=d;
+		return candidate.floatValue();
 	}
-	public static int selectMedianY(ArrayList<Point2D> points, int axis){
-		//TODO
-		return 0;
+	private static float getMin(ArrayList<Double> possibleMins) {
+		Double candidate=possibleMins.get(0);
+		for(Double d: possibleMins)
+			if(d<candidate)
+				candidate=d;
+		return candidate.floatValue();
 	}
 
 }
