1e771500b4e341b469a3d55cab822d8e29dc579b
raticate <caterina.munoz.v@gmail.com>
2014-06-03 15:30:45 -0400

    terminado lo de mem. secundaria, ver si funca

--- src/KdTreeImplementation/MemoryNode.java
+++ src/KdTreeImplementation/MemoryNode.java
@@ -3,26 +3,30 @@ package KdTreeImplementation;
 import java.awt.geom.Point2D;
 import java.awt.geom.Point2D.Double;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 
 public class MemoryNode {
-	private final int bufferSize=42;
+	private final static int bufferSize=42;
 	private long rightPos;
 	private long leftPos;
 	private double x;
 	private double y;
 	private char isLeaf;
 	private long pos;
-	MemoryManager manager;
-	public MemoryNode(double x, double y, char leaf, MemoryManager man) throws IOException{
+	private static MemoryManager manager;
+	public MemoryNode(double x, double y, char leaf) throws IOException{
 		isLeaf=leaf;
 		this.x=x;
 		this.y=y;
-		this.manager=man;
 		this.pos=manager.getNewPosition();
 		//Como el archivo no puede tener posiciones negativas, un numero negativo es el null
 		rightPos=-1;
 		leftPos=-1;
 		this.manager.writeBuff(this.toBuff(), this.pos);
+	}
+	//esto hay que hacerlo apenas esté creado el memory manager
+	public static void setMemoryManager(MemoryManager man){
+		manager=man;
 	}
 	public void setRight(long pos) throws IOException {
 		this.rightPos=pos;
@@ -31,20 +35,50 @@ public class MemoryNode {
 	}
 
  
-	private static byte[] toBuff() {
-		// TODO Auto-generated method stub
-		return null;
+	private byte[] toBuff() {
+		//orden de nodo: posLeft, pos, posRight, x, y, isleaf
+		byte[] b=new byte[bufferSize];
+		ByteBuffer buff = null;
+		buff.wrap(b);
+		buff.position(0);
+		buff.putLong(this.leftPos);
+		buff.putLong(this.pos);
+		buff.putLong(this.rightPos);
+		buff.putDouble(this.x);
+		buff.putDouble(this.y);
+		buff.putChar(this.isLeaf);
+		buff.position(0);
+		buff.get(b);
+		return b;
 	}
+	
+	private static MemoryNode getNodeFromBuff(byte[] b) throws IOException {
+		//orden de nodo: posLeft, pos, posRight, x, y, isleaf
+		ByteBuffer buff = null;
+		buff.wrap(b);
+		buff.position(0);
+		long posLeft, posRight, pos;
+		double x,y;
+		char isLeaf;
+		posLeft=buff.getLong();
+		pos=buff.getLong();
+		posRight=buff.getLong();
+		x=buff.getDouble();
+		y=buff.getDouble();
+		isLeaf=buff.getChar();
+		MemoryNode n= new MemoryNode(x, y, isLeaf);
+		n.setLeft(posLeft);
+		n.setRight(posRight);
+		return n;
+	}
+	
 	public MemoryNode getRight() throws IOException {
 		byte[]b= this.manager.readBuff(rightPos);
 		return this.getNodeFromBuff(b);
 	}
 
 
-	private static MemoryNode getNodeFromBuff(byte[] b) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+
 	
 	public void setLeft(long pos) throws IOException {
 		this.leftPos=pos;
