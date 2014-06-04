11d9fd7904bc97e4373de9bd8e8ee2ac3a65eb0c
Cate <caterina.munoz.v@gmail.com>
2014-06-03 22:51:48 -0400

    implementadas las instancias
    hay que descargar y agregar la librería math3

--- src/KdTreeImplementation/InstanceGenerator.java
+++ src/KdTreeImplementation/InstanceGenerator.java
@@ -1,11 +1,12 @@
 package KdTreeImplementation;
 
 import java.awt.geom.Point2D;
-import org.apache.commons.math3.random;
 import java.util.ArrayList;
 import java.util.Random;
+import org.apache.commons.math3.random.SobolSequenceGenerator;
+import org.apache.commons.math.exception.OutOfRangeException;
 
-public class InstanceGenerator {
+public class InstanceGenerator{
 	final static private double c=0.7;
 	Random r;
 	SobolSequenceGenerator sobol;
@@ -23,12 +24,15 @@ public class InstanceGenerator {
 		}
 		return ret;
 	}
-	public ArrayList<Point2D.Double> lowDiscrepancyInstance(int n){
+	public ArrayList<Point2D.Double> lowDiscrepancyInstance(int n) throws OutOfRangeException{
 		double max=Math.sqrt(n)*c;
 		ArrayList<Point2D.Double> ret= new ArrayList<Point2D.Double>();
+		sobol=new SobolSequenceGenerator(2);
+		double[] array=new double[2];
 		for(int i=0;i<n;i++){
-			double x=0; //TODO
-			double y=0;
+			array=sobol.nextVector();
+			double x=array[0]; //TODO
+			double y=array[1];
 			Point2D.Double newPoint=new Point2D.Double(x,y);
 			ret.add(newPoint);
 		}
